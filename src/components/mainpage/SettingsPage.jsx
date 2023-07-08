3a4ea6618c6627676bed7f7819d800205ff65cdd
Kevin Yao <kevinyao2003@hotmail.com>
2023-07-08 23:32:20 +1200

    Update SettingsPage.jsx

--- src/components/mainpage/SettingsPage.jsx
+++ src/components/mainpage/SettingsPage.jsx
@@ -1,13 +1,20 @@
-import { Grid } from "@mui/material";
 import React from "react";
+import Profile from "../portfoliopage/Profile";
+import { UserContext } from "../../App";
+import { fetchUserData } from "../../utils/fetchUserData";
 
 
 const SettingsPage = () => {
+  const { user } = React.useContext(UserContext);
+  const userdata = fetchUserData(user.email);
+  const email = userdata.email;
+
+
 
   return (
-    <div>
-    <p> hi</p>
-    </div>
+    <main>
+    <p>{email}</p>
+    </main>
   );
 };
 
